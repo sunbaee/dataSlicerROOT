3fee4ac9771652e3d51cc629d519fc42d235a8ae
sunbaee._ <ehor.stunned718@passfwd.com>
2025-04-19 17:16:14 -0300

    converting all data to numbers in a list

--- Train/train.py
+++ Train/train.py
@@ -2,53 +2,62 @@
 # std::regexp symbols that are exported by cppyy.
 # See also: https://github.com/wlav/cppyy/issues/227
 from xgboost import XGBClassifier;
-import ROOT;
+from ROOT import RDataFrame, gSystem, TMVA;
 
 import numpy as np;
 import json;
- 
+
+# Converts ROOT data to list with numpy arrays
 def ConvertData(item):
-    # Exits if current item is a number.
+    # Exits if current item is a real number.
     if ( type(item) == float or type(item) == int ): return item;
 
-    # Converts ROOT Vectors to numpy arrays and returns.
-    if ( type(item) != list and type(item) != np.ndarray ): 
-        print(type(item));
-        return np.array([x for x in item]);
+    if ( type(item) == list or type(item) == np.ndarray ): 
+        newItem = [];
+        for i,_ in enumerate(item): newItem.append(ConvertData(item[i]));
+        
+        # Returns lists or arrays inside item.
+        return newItem;
 
-    newItem = [];
-    for i,_ in enumerate(item): newItem.append(ConvertData(item[i]));
+    # Converts ROOT Vectors to numpy arrays by iterating them and returns.
 
-    # Returns lists or arrays inside item.
-    return newItem;
+    # Finding type of vector
+    typeStr = type(item).__cpp_name__;
+    vecTypeIdx = typeStr.find('<');
 
+    vecType = typeStr[vecTypeIdx + 1 : -1];
+
+    if (vecType == "float" or vecType == "int"): return np.asarray([x for x in item]);
+
+    # Vector Type is a Lorentz Vector.
+    return np.asarray([np.asarray([x.Px(), x.Py(), x.Pz(), x.E()]) for x in item]);
+
+# Loads data from root files
 def LoadData(signalFile, backgroundFile):
     # Read data from ROOT files (training).
-    data_sig = ROOT.RDataFrame("tree", signalFile).AsNumpy();
-    data_bkg = ROOT.RDataFrame("tree", backgroundFile).AsNumpy();
+    data_sig = RDataFrame("tree", signalFile).AsNumpy();
+    data_bkg = RDataFrame("tree", backgroundFile).AsNumpy();
 
     with open("variables.json", "r") as file: allVariables = json.load(file);
-
-    # TODO: Convert variables values to float (RVec<Int>).
 
     # Getting variable data from data frame.
     sigData = np.vstack([data_sig[var] for var in allVariables[0]]).T;
     bkgData = np.vstack([data_bkg[var] for var in allVariables[1]]).T;
 
-    print(sigData, "\n\n\n");
-    sigData = ConvertData(sigData);
-    print("\n\n\n", sigData);
-    exit();
-
     # Convert inputs to format readable by machine learning tools
-    x_sig = np.array([x for x in sigData]);
-    x_bkg = np.array([x for x in bkgData]);
+    xSig = ConvertData(sigData);
+    xBkg = ConvertData(bkgData);
 
-    x = np.vstack([x_sig, x_bkg]);
+    print(xSig, "\n\n\n", xBkg);
+
+    #xSig = np.array([x for x in sigData]);
+    #xBkg = np.array([x for x in bkgData]);
+
+    x = np.vstack([xSig, xBkg]);
 
     # Create labels
-    num_sig = x_sig.shape[0];
-    num_bkg = x_bkg.shape[0];
+    num_sig = xSig.shape[0];
+    num_bkg = xBkg.shape[0];
 
     y = np.hstack([np.ones(num_sig), np.zeros(num_bkg)]);
  
@@ -60,7 +69,7 @@ def LoadData(signalFile, backgroundFile):
  
 if __name__ == "__main__":
     # Loading dictionaries.
-    ROOT.gSystem.Load("libdict.so");
+    gSystem.Load("libdict.so");
 
     # Load data.
     x, y, w = LoadData("train_signal.root", "train_background.root");
@@ -71,5 +80,5 @@ if __name__ == "__main__":
  
     # Save model in TMVA format
     print("Training done on ", x.shape[0], "events. Saving model in model.root");
-    ROOT.TMVA.Experimental.SaveXGBoost(bdt, "myBDT", "model.root", num_inputs=x.shape[1]);
+    TMVA.Experimental.SaveXGBoost(bdt, "myBDT", "model.root", num_inputs=x.shape[1]);
 
