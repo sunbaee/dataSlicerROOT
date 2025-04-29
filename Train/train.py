7d1e999c7e1c3ecac2e8641d1765ff64a8001ef4
sunbaee._ <ehor.stunned718@passfwd.com>
2025-04-29 14:15:08 -0300

    changing x, y, w to be arrays computed for each column in root data frame

--- Train/train.py
+++ Train/train.py
@@ -1,14 +1,20 @@
 # XGBoost has to be imported before ROOT to avoid crashes because of clashing
 # std::regexp symbols that are exported by cppyy.
 # See also: https://github.com/wlav/cppyy/issues/227
-from xgboost import XGBClassifier;
+from sklearn.preprocessing import OneHotEncoder;
+from xgboost import XGBClassifier, DMatrix;
+
 from ROOT import RDataFrame, gSystem, TMVA;
 
+import pandas as pd;
 import numpy as np;
 import json;
 
 # Converts ROOT data to list with numpy arrays
 def ConvertData(item):
+    # TODO: Make this function faster (np arrays)
+    # TODO: Remove list with no information (*list of list of lists)
+
     # Exits if current item is a real number.
     if ( type(item) == float or type(item) == int ): return item;
 
@@ -32,82 +38,52 @@ def ConvertData(item):
     # Vector Type is a Lorentz Vector.
     return np.asarray([np.asarray([x.Px(), x.Py(), x.Pz(), x.E()]) for x in item]);
 
-def Flatten(listSig, listBkg):
-    numVariables = len(listSig);
-    if (numVariables != len(listBkg)): return None;
-
-    oldLists = [listSig, listBkg]
-    newLists = [[], []];
-    for i,_ in enumerate(listSig):
-        curType = type(listSig[i]);
-        if (curType != type(listBkg[i])): return None;
-        
-        if (curType != np.ndarray): 
-            for j, curList in enumerate(newLists): curList.append(oldLists[j][i]);
-            continue;
-
-        maxLength = max(len(listSig[i]), len(listBkg[i]));
-        for j, curList in enumerate(newLists):
-            for element in oldLists[j][i][: maxLength - 1]: 
-                # TODO: can have arrays here.
-                print(element);
-                if (element != np.ndarray): curList.append(element); continue;
-                
-                for k in element: 
-                    curList.append(k);
-    
-#    print(newLists[0], "\n\n", newLists[1]);
-    return np.asarray(newLists[0]), np.asarray(newLists[1]);
-
 # Loads data from root files
 def LoadData(signalFile, backgroundFile):
-    print(">>> Reading data...")
-    # Read data from ROOT files (training).
-    data_sig = RDataFrame("tree", signalFile).AsNumpy();
-    data_bkg = RDataFrame("tree", backgroundFile).AsNumpy();
-
-    print(">>> Data loaded.")
+    print(">>> Reading data...");
 
     with open("variables.json", "r") as file: allVariables = json.load(file);
 
+    # Read data from ROOT files (training).
+    rootDataSig = RDataFrame("tree", signalFile).AsNumpy();
+    rootDataBkg = RDataFrame("tree", backgroundFile).AsNumpy();
+
+    print(">>> Data loaded.");
+
     # Getting variable data from data frame.
-    sigData = np.vstack([data_sig[var] for var in allVariables[0]]).T;
-    bkgData = np.vstack([data_bkg[var] for var in allVariables[1]]).T;
+    sigData = np.vstack([rootDataSig[var] for var in allVariables[0]]).T;
+    bkgData = np.vstack([rootDataBkg[var] for var in allVariables[1]]).T;
 
     # Convert inputs to format readable by machine learning tools
-    xSigRaw, xBkgRaw = ConvertData(sigData), ConvertData(bkgData);
-    print("Converted");
+    sigRaw, bkgRaw = ConvertData(sigData), ConvertData(bkgData);
+    print(">>> Converted");
 
-    flatDatas = Flatten(xSigRaw, xBkgRaw);
-    if (not flatDatas): return None; 
-    xSig, xBkg = flatDatas;
-    print("Flattened.")
+    print(sigRaw, "\n\n", bkgRaw);
 
-    print(xSig, "\n\n", xBkg);
+    idxArr = range(len(allVariables));
 
-    x = np.vstack([xSig, xBkg]);
+    # Stacking each variable separately
+    xValues = [np.vstack([sigRaw[idx], bkgRaw[idx]]) for idx in idxArr];
 
-    # Create labels
-    num_sig = xSig.shape[0];
-    num_bkg = xBkg.shape[0];
+    # Create labels for each variable 
+    numSigs = [sigRaw[idx].shape[0] for idx in idxArr];
+    numBkgs = [bkgRaw[idx].shape[0] for idx in idxArr];
 
-    y = np.hstack([np.ones(num_sig), np.zeros(num_bkg)]);
+    yValues = [ np.hstack([np.ones(numSigs[idx]), np.zeros(numBkgs[idx])]) for idx in idxArr];
  
-    # Compute weights balancing both classes
-    num_all = num_sig + num_bkg;
-    w = np.hstack([np.ones(num_sig) * num_all / num_sig, np.ones(num_bkg) * num_all / num_bkg]);
+    # Compute weights balancing both classes for each variable
+    numAll = numSigs + numBkgs;
+    wValues = [np.hstack([ np.ones(numSigs[idx]) * numAll[idx] / numSigs[idx] , 
+                     np.ones(numBkgs[idx]) * numAll[idx] / numBkgs[idx] ]) for idx in idxArr];
  
-    return x, y, w;
+    return xValues, yValues, wValues;
  
 if __name__ == "__main__":
     # Loading dictionaries.
     gSystem.Load("libdict.so");
 
     # Load data.
-    trainData = LoadData("train_signal.root", "train_background.root");
-    if (not trainData): print("An error occurred."); exit();
-
-    x, y, w = trainData;
+    x, y, w = LoadData("train_signal.root", "train_background.root");
  
     # Fit xgboost model
     bdt = XGBClassifier(max_depth=3, n_estimators=500);
